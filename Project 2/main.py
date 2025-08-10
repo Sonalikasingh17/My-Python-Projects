766fe0a99ba5b527ca48ee192a1952996d16406b
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-08-10 12:40:04 +0530

    Update main.py

--- Project 2/main.py
+++ Project 2/main.py
@@ -5,7 +5,7 @@ When the user guesses the correct number, the program displays the
 number of guesses the player used to arrive at the number.'''
   
 import random
-n = random.randint(15, 98)
+n = random.randint(15, 98) 
 
 a = -1
 guesses = 0
@@ -85,6 +85,7 @@ This code will allow the user to play a simple number guessing game, with the pr
     
 
     
+
 
 
 
